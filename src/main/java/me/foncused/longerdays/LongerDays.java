f1f3a2b73a892bab20bec4a705a446d51a879098
pupskuchen <code@pkn.pw>
2023-03-25 00:57:40 +0100

    chore: format code

--- src/main/java/me/foncused/longerdays/LongerDays.java
+++ src/main/java/me/foncused/longerdays/LongerDays.java
@@ -11,58 +11,59 @@ import org.bukkit.scheduler.BukkitRunnable;
 
 public class LongerDays extends JavaPlugin {
 
-	public static final String PREFIX = "[LongerDays] ";
+    public static final String PREFIX = "[LongerDays] ";
 
-	private ConfigManager cm;
+    private ConfigManager cm;
 
-	@Override
-	public void onEnable() {
-		this.registerConfig();
-		this.registerEvents();
-		new BukkitRunnable() {
-			@Override
-			public void run() {
-				setDaylightCycle(false);
-				registerRunnables();
-			}
-		}.runTask(this);
-	}
+    @Override
+    public void onEnable() {
+        this.registerConfig();
+        this.registerEvents();
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                setDaylightCycle(false);
+                registerRunnables();
+            }
+        }.runTask(this);
+    }
 
-	@Override
-	public void onDisable() {
-		this.setDaylightCycle(true);
-	}
+    @Override
+    public void onDisable() {
+        this.setDaylightCycle(true);
+    }
 
-	private void registerConfig() {
-		this.saveDefaultConfig();
-		this.cm = new ConfigManager(this.getConfig());
-		this.cm.validate();
-	}
+    private void registerConfig() {
+        this.saveDefaultConfig();
+        this.cm = new ConfigManager(this.getConfig());
+        this.cm.validate();
+    }
 
-	private void registerEvents() {
-		this.getServer().getPluginManager().registerEvents(new PlayerBed(getConfigManager()), this);
-	}
+    private void registerEvents() {
+        this.getServer().getPluginManager().registerEvents(new PlayerBed(getConfigManager()), this);
+    }
 
-	private void registerRunnables() {
-		final Runnable runnable = new Runnable(this);
-		Bukkit.getWorlds()
-				.stream()
-				.filter(world -> this.cm.getWorlds().contains(world.getName()))
-				.forEach(runnable::runCycles);
-	}
+    private void registerRunnables() {
+        final Runnable runnable = new Runnable(this);
+        Bukkit.getWorlds()
+                .stream()
+                .filter(world -> this.cm.getWorlds().contains(world.getName()))
+                .forEach(runnable::runCycles);
+    }
 
-	private void setDaylightCycle(final boolean value) {
-		Bukkit.getWorlds()
-				.stream()
-				.filter(world -> this.cm.getWorlds().contains(world.getName()))
-				.forEach(world -> {
-					world.setGameRule(GameRule.DO_DAYLIGHT_CYCLE, value);
-					LongerDaysUtil.console("Setting GameRule.DO_DAYLIGHT_CYCLE to " + value + " for world '" + world.getName() + "'");
-				});
-	}
+    private void setDaylightCycle(final boolean value) {
+        Bukkit.getWorlds()
+                .stream()
+                .filter(world -> this.cm.getWorlds().contains(world.getName()))
+                .forEach(world -> {
+                    world.setGameRule(GameRule.DO_DAYLIGHT_CYCLE, value);
+                    LongerDaysUtil.console(
+                            "Setting GameRule.DO_DAYLIGHT_CYCLE to " + value + " for world '" + world.getName() + "'");
+                });
+    }
 
-	public ConfigManager getConfigManager() {
-		return this.cm;
-	}
+    public ConfigManager getConfigManager() {
+        return this.cm;
+    }
 
 }
--- src/main/java/me/foncused/longerdays/config/ConfigManager.java
+++ src/main/java/me/foncused/longerdays/config/ConfigManager.java
@@ -10,74 +10,74 @@ import java.util.Set;
 
 public class ConfigManager {
 
-	private final FileConfiguration config;
-	private int day;
-	private int night;
-	private Set<String> worlds;
+    private final FileConfiguration config;
+    private int day;
+    private int night;
+    private Set<String> worlds;
 
-	private boolean nightSkippingEnabled;
-	private boolean percentageEnabled;
-	private int percentage;
+    private boolean nightSkippingEnabled;
+    private boolean percentageEnabled;
+    private int percentage;
 
+    public ConfigManager(final FileConfiguration config) {
+        this.config = config;
+    }
 
-	public ConfigManager(final FileConfiguration config) {
-		this.config = config;
-	}
+    public void validate() {
 
-	public void validate() {
+        // day
+        final int day = this.config.getInt("day", 30);
+        if (day <= 0) {
+            this.day = 30;
+            LongerDaysUtil.consoleWarning("Set day cycle to " + day + " minutes is not safe, reverting to default...");
+        } else {
+            this.day = day;
+        }
+        LongerDaysUtil.console("Set day cycle to " + this.day + " minutes");
 
-		// day
-		final int day = this.config.getInt("day", 30);
-		if(day <= 0) {
-			this.day = 30;
-			LongerDaysUtil.consoleWarning("Set day cycle to " + day + " minutes is not safe, reverting to default...");
-		} else {
-			this.day = day;
-		}
-		LongerDaysUtil.console("Set day cycle to " + this.day + " minutes");
+        // night
+        final int night = this.config.getInt("night", 5);
+        if (night <= 0) {
+            this.night = 5;
+            LongerDaysUtil
+                    .consoleWarning("Set night cycle to " + night + " minutes is not safe, reverting to default...");
+        } else {
+            this.night = night;
+        }
+        LongerDaysUtil.console("Set night cycle to " + this.night + " minutes");
 
-		// night
-		final int night = this.config.getInt("night", 5);
-		if(night <= 0) {
-			this.night = 5;
-			LongerDaysUtil.consoleWarning("Set night cycle to " + night + " minutes is not safe, reverting to default...");
-		} else {
-			this.night = night;
-		}
-		LongerDaysUtil.console("Set night cycle to " + this.night + " minutes");
+        // worlds
+        final List<String> worlds = this.config.getStringList("worlds");
+        this.worlds = new HashSet<>();
+        this.worlds.addAll(worlds);
+        this.worlds = Collections.unmodifiableSet(this.worlds);
 
-		// worlds
-		final List<String> worlds = this.config.getStringList("worlds");
-		this.worlds = new HashSet<>();
-		this.worlds.addAll(worlds);
-		this.worlds = Collections.unmodifiableSet(this.worlds);
+        percentageEnabled = this.config.getBoolean("players-sleeping-percentage.enabled");
+        percentage = this.config.getInt("players-sleeping-percentage.percentage");
+        nightSkippingEnabled = this.config.getBoolean("night-skipping.enabled");
+    }
 
-		percentageEnabled =  this.config.getBoolean("players-sleeping-percentage.enabled");
-		percentage = this.config.getInt("players-sleeping-percentage.percentage");
-		nightSkippingEnabled = this.config.getBoolean("night-skipping.enabled");
-	}
+    public int getDay() {
+        return this.day;
+    }
 
-	public int getDay() {
-		return this.day;
-	}
+    public int getNight() {
+        return this.night;
+    }
 
-	public int getNight() {
-		return this.night;
-	}
+    public Set<String> getWorlds() {
+        return Collections.unmodifiableSet(this.worlds);
+    }
 
-	public Set<String> getWorlds() {
-		return Collections.unmodifiableSet(this.worlds);
-	}
+    public boolean isPercentageEnabled() {
+        return percentageEnabled;
+    }
 
-	public boolean isPercentageEnabled() {
-		return percentageEnabled;
-	}
+    public int getPercentage() {
+        return percentage;
+    }
 
-	public int getPercentage() {
-		return percentage;
-	}
-
-	public boolean isNightSkippingEnabled() {
-		return nightSkippingEnabled;
-	}
+    public boolean isNightSkippingEnabled() {
+        return nightSkippingEnabled;
+    }
 }
