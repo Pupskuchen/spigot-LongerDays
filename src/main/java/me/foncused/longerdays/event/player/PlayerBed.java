b5954f20cc4b01c5cdff03f664aeab949a5a2ef7
pupskuchen <code@pkn.pw>
2023-03-25 02:16:20 +0100

    fix: improve sleeping player counting

--- src/main/java/me/foncused/longerdays/event/player/PlayerBed.java
+++ src/main/java/me/foncused/longerdays/event/player/PlayerBed.java
@@ -7,12 +7,10 @@ import org.bukkit.World;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerBedEnterEvent;
-import org.bukkit.event.player.PlayerBedLeaveEvent;
 
 public class PlayerBed implements Listener {
 
     private final ConfigManager configManager;
-    private int sleeping;
 
     public PlayerBed(ConfigManager configManager) {
         this.configManager = configManager;
@@ -20,14 +18,16 @@ public class PlayerBed implements Listener {
 
     @EventHandler
     public void onPlayerBedEnter(final PlayerBedEnterEvent event) {
-        this.sleeping++;
-
-        // do nothing if night skipping is disabled
         if (!configManager.isNightSkippingEnabled()) {
             return;
         }
 
+        if (event.getBedEnterResult() != PlayerBedEnterEvent.BedEnterResult.OK) {
+            return;
+        }
+
         final World world = event.getPlayer().getWorld();
+        final int sleeping = (int) world.getPlayers().stream().filter((player) -> player.isSleeping()).count() + 1;
 
         int percentage;
 
@@ -42,20 +42,11 @@ public class PlayerBed implements Listener {
         }
 
         percentage = configManager.getPercentage();
-        if (LongerDaysUtil.isNight(world)
-                && event.getBedEnterResult() == PlayerBedEnterEvent.BedEnterResult.OK
-                && (this.sleeping / world.getPlayers().size()) * 100 >= percentage) {
-            this.sleeping = 0;
+
+        if ((sleeping / world.getPlayers().size()) * 100 >= percentage) {
             world.setTime(1000);
             event.setCancelled(true);
             LongerDaysUtil.console("The night has been skipped by sleeping");
-        }
-    }
-
-    @EventHandler
-    public void onPlayerBedLeave(final PlayerBedLeaveEvent event) {
-        if (this.sleeping > 0) {
-            this.sleeping--;
         }
     }
 
